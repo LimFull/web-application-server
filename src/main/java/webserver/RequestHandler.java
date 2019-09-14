49d4feb6cc686ffd685bc27b39218784402ddb76
임가득 <full7002@naver.com>
2019-09-14 15:01:55 +0900

    회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import util.HttpRequestUtils;
+import util.IOUtils;
 import model.User;
 
 public class RequestHandler extends Thread {
@@ -34,6 +35,7 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
         	BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
         	String line = br.readLine();
+        	
         	log.debug("request line = {}", line);
         	
         	if (line == null) {
@@ -41,30 +43,54 @@ public class RequestHandler extends Thread {
         	}
         	
         	String[] tokens = line.split(" ");
+        	int contentLength = 0;
         	
         	while (!"".equals(line)) {
         		line = br.readLine();
         		log.debug("header : {}", line);
+        		if (line.contains("Content-Length")) {
+        			contentLength = getContentLength(line);
+        		}
         	}
+        	
+        	//log.debug("본문 : {}", br.readLine());
         	String url = tokens[1];
-        	if (url.startsWith("/user/create")) {
-        		int index = url.indexOf("?");
-        		String queryString = url.substring(index+1);
-        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+        	if (url.equals("/user/create")) {
+        		String body = IOUtils.readData(br, contentLength);
+        		log.debug("body = {}", body);
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(body);
         		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
         		log.debug("User : {}", user);
-        	}else {
+        		DataOutputStream dos = new DataOutputStream(out);
+        		response302Header(dos, "/index.html");
+        		
+        		
+        	} else{
 	            DataOutputStream dos = new DataOutputStream(out);
 	            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
 	            //byte[] body = "라즈베리로봇 서버".getBytes();
-	            response200Header(dos, body.length, tokens[1]);
+	            response200Header(dos, body.length, url);
 	            responseBody(dos, body);
         	}
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+    private int getContentLength(String content) {
+    	String[] lengthTokens = content.split(":");
+    	return Integer.parseInt(lengthTokens[1].trim());
+    }
+    
+    private void response302Header(DataOutputStream dos, String url) {
+    	try {
+            dos.writeBytes("HTTP/1.1 302 Found\r\n");
+            dos.writeBytes("Location: "+url+"\r\n");
+            dos.writeBytes("\r\n");
+	    }catch (IOException e) {
+    		log.error(e.getMessage());
+    	}
 
+    }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String token) {
     	try {
 	    	if (token.endsWith(".css")){
