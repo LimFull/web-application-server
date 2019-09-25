58c9a76163ccffa596fbbbcc90e39d8eac4b4fff
임가득 <full7002@naver.com>
2019-09-25 21:57:23 +0900

    분기문 리팩토링

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -40,55 +40,12 @@ public class RequestHandler extends Thread {
         	log.debug("만들어진 path = {}", path);
         	HttpResponse response = new HttpResponse(out);
         	
-        	
         	if (path.equals("/user/create")) {
-        		User user = new User(
-        				request.getParameter("userId"),
-        				request.getParameter("password"),
-        				request.getParameter("name"),
-        				request.getParameter("email"));
-        		
-        		log.debug("User : {}", user);
-        		DataBase.addUser(user);
-        		DataOutputStream dos = new DataOutputStream(out);
-        		response.sendRedirect("/index.html");
-        		
-        		
+        		createUser(request, response);
         	}else if (path.equals("/user/login")){
-        		User user = DataBase.findUserById(request.getParameter("userId"));
-        		if (user == null) {
-        			response.sendRedirect("/user/login_failed.html");
-        			return;
-        		}
-        		
-        		if (user.getPassword().equals(request.getParameter("password"))){
-        			DataOutputStream dos = new DataOutputStream(out);
-        	
-        			response.setHeader("Set-Cookie", "logined = true");
-        			response.sendRedirect("/index.html");
-        		}else {
-        			response.sendRedirect("/user/login_failed.html");
-        		}
-        		
+        		login(request, response);
         	}else if (path.equals("/user/list")) {
-        		if (!isLogin(request.getHeader("Cookie"))) {
-        			response.sendRedirect("/user/login.html");
-        			
-        			return;
-        		}
-        		Collection<User> users = DataBase.findAll();
-        		StringBuilder sb = new StringBuilder();
-        		sb.append("<table border='1'>");
-        		for (User user : users) {
-        			sb.append("<tr>");
-        			sb.append("<td>" + user.getUserId() + "</td>");
-        			sb.append("<td>" + user.getName() +  "</td>");
-        			sb.append("<td>" + user.getEmail() +  "</td>");
-        			sb.append("</tr>");
-        		}
-        		sb.append("</table>");
-        		response.forwardBody(sb.toString());
-        		
+        		listUser(request, response);
         	}
         	else{
         		response.forward(path);
@@ -96,6 +53,52 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    private void listUser(HttpRequest request, HttpResponse response) {
+    	if (!isLogin(request.getHeader("Cookie"))) {
+			response.sendRedirect("/user/login.html");
+			
+			return;
+		}
+		Collection<User> users = DataBase.findAll();
+		StringBuilder sb = new StringBuilder();
+		sb.append("<table border='1'>");
+		for (User user : users) {
+			sb.append("<tr>");
+			sb.append("<td>" + user.getUserId() + "</td>");
+			sb.append("<td>" + user.getName() +  "</td>");
+			sb.append("<td>" + user.getEmail() +  "</td>");
+			sb.append("</tr>");
+		}
+		sb.append("</table>");
+		response.forwardBody(sb.toString());
+    }
+    private void login(HttpRequest request, HttpResponse response) {
+    	User user = DataBase.findUserById(request.getParameter("userId"));
+		if (user == null) {
+			response.sendRedirect("/user/login_failed.html");
+			return;
+		}
+		
+		if (user.getPassword().equals(request.getParameter("password"))){
+			response.setHeader("Set-Cookie", "logined = true");
+			response.sendRedirect("/index.html");
+		}else {
+			response.sendRedirect("/user/login_failed.html");
+		}
+    }
+    private void createUser(HttpRequest request, HttpResponse response) {
+    	User user = new User(
+				request.getParameter("userId"),
+				request.getParameter("password"),
+				request.getParameter("name"),
+				request.getParameter("email"));
+		
+		log.debug("User : {}", user);
+		DataBase.addUser(user);
+		DataOutputStream dos = new DataOutputStream(out);
+		response.sendRedirect("/index.html");
+		
     }
     private String getDefaultPath(String path) {
     	if (path.equals("/")) {
